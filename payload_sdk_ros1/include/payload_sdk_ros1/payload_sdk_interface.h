9a6f13613fa286a7774dc1645f64fd8f371bdc46
Gaiwqqq <1926038838@qq.com>
2025-04-14 21:39:36 +0800

    first commit

---
+++ payload_sdk_ros1/include/payload_sdk_ros1/payload_sdk_interface.h
@@ -0,0 +1,41 @@
+//
+// Created by gwq on 25-4-14.
+//
+
+#ifndef PAYLOAD_SDK_INTERFACE_H
+#define PAYLOAD_SDK_INTERFACE_H
+
+#include "ros/ros.h"
+#include <Eigen/Eigen>
+
+#include <liveview/test_liveview_entry.hpp>
+#include <perception/test_perception_entry.hpp>
+#include <flight_control/test_flight_control.h>
+#include <gimbal/test_gimbal_entry.hpp>
+#include "application.hpp"
+#include "fc_subscription/test_fc_subscription.h"
+#include <gimbal_emu/test_payload_gimbal_emu.h>
+#include <camera_emu/test_payload_cam_emu_media.h>
+#include <camera_emu/test_payload_cam_emu_base.h>
+#include <dji_logger.h>
+#include "widget/test_widget.h"
+#include "widget/test_widget_speaker.h"
+#include <power_management/test_power_management.h>
+#include "data_transmission/test_data_transmission.h"
+#include <flight_controller/test_flight_controller_entry.h>
+#include <positioning/test_positioning.h>
+#include <hms_manager/hms_manager_entry.h>
+#include "camera_manager/test_camera_manager_entry.h"
+
+class PayloadSdkInterface{
+public:
+    PayloadSdkInterface();
+    ~PayloadSdkInterface();
+
+
+private:
+    ros::NodeHandle nh_;
+};
+
+
+#endif //PAYLOAD_SDK_INTERFACE_H
